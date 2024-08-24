8f455efca6616f461f92d5442f60bace4a8be6cf
Abdel-manan Abdel-rahman <abdelmanan.abdelrahman03@gmail.com>
2024-08-24 19:37:00 +0300

    train file vanished!! how!!

---
+++ optimizers/sgd.py
@@ -0,0 +1,41 @@
+import torch
+from typing import Dict, Any, List, Optional
+
+
+# https://www.geeksforgeeks.org/custom-optimizers-in-pytorch/
+class CustomOptimizer(torch.optim.Optimizer): 
+    def __init__(self, params, lr=1e-3):
+        defaults = dict(lr=lr) 
+        super().__init__(params, defaults) 
+  
+    def step(self, closure=None)->Any: 
+        if closure is not None: 
+            with torch.enable_grad(): 
+                closure()
+
+        for group in self.param_groups: 
+            for p in group['params']: 
+                if p.grad is None: 
+                    continue
+                #p.data -= group['lr']*p.grad.data
+                p.data.add_(-p['lr'], p.grad)
+  
+
+class StochasticGradientDescent(torch.optim.Optimizer): 
+    def __init__(self, params, lr=1e-3):
+        defaults = dict(lr=lr) 
+        super(StochasticGradientDescent, self).__init__(params, defaults) 
+  
+    def step(self, closure=None)->Any: 
+        if closure is not None: 
+            with torch.enable_grad(): 
+                closure()
+
+        for group in self.param_groups: 
+            for p in group['params']: 
+                if p.grad is None: 
+                    continue
+                p.data.add_(p.grad, alpha=-group['lr'])
+
+
+
